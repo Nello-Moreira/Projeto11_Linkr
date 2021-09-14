1875c987b5d0d930bf1cecdebcc2654bc3dd1490
Nello-Moreira <g.moreira1@outlook.com>
2021-09-14 11:19:05 -0300

    fixed: routes import v2

---
+++ src/routes/routes.js
@@ -0,0 +1,11 @@
+const routes = {
+    login: '/',
+    signUp: '/cadastro',
+    timeline: '/timeline',
+    myPosts: '/my-posts',
+    user: '/user/:id',
+    likes: '/my-likes',
+    trending: '/hashtag/:hashtag'
+}
+
+export default routes;
