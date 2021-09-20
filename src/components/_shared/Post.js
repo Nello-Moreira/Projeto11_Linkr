b93a50c0a7f7aa62f88623d572904f760c61064d
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-09-20 09:55:34 -0300

    Removed console.logs

--- src/components/_shared/Post.js
+++ src/components/_shared/Post.js
@@ -9,12 +9,13 @@ import routes from "../../routes/routes";
 import { useContext, useEffect, useState, useRef } from "react";
 import UserContext from "../../contexts/UserContext";
 import { likePost, dislikePost, edit } from "../../API/requests";
-import ReactTooltip from 'react-tooltip';
+import ReactTooltip from "react-tooltip";
 import autosize from "autosize";
-import PagePostsContext from '../../contexts/PagePostsContext';
+import PagePostsContext from "../../contexts/PagePostsContext";
 
 export default function Post({ postData }) {
-	const { id, text, link, linkTitle, linkDescription, linkImage, user, likes } = postData;
+	const { id, text, link, linkTitle, linkDescription, linkImage, user, likes } =
+		postData;
 
 	const { loggedUser } = useContext(UserContext);
 	const { setDeletingPostId } = useContext(PagePostsContext);
@@ -23,19 +24,23 @@ export default function Post({ postData }) {
 
 	const [likesNumber, setLikesNumber] = useState(likes.length);
 
-	const likesText = likesNumber > 0 ? (likesNumber > 1 ? `${likesNumber} likes` : `${likesNumber} like`) : "";
+	const likesText =
+		likesNumber > 0
+			? likesNumber > 1
+				? `${likesNumber} likes`
+				: `${likesNumber} like`
+			: "";
 
 	const [tooltip, setTooltip] = useState("");
 
 	useEffect(() => {
 		constructTooltip();
-	}, [tooltip, isLiked, likesNumber, likesText])
+	}, [tooltip, isLiked, likesNumber, likesText]);
 
 	useEffect(() => {
-		if (likes.find(like => like.userId === loggedUser.user.id))
+		if (likes.find((like) => like.userId === loggedUser.user.id))
 			setIsLiked(true);
 	}, []);
-
 
 	function constructTooltip() {
 		let tooltipNumber = likesNumber;
@@ -44,8 +49,7 @@ export default function Post({ postData }) {
 		if (likesNumber > 0) {
 			if (isLiked) {
 				tmpTooltip = "Você ";
-			}
-			else {
+			} else {
 				tmpTooltip = `${likes[0]["user.username"]} `;
 			}
 			tmpTooltip = moreUsersLiked(tmpTooltip, tooltipNumber);
@@ -61,20 +65,17 @@ export default function Post({ postData }) {
 				if (isLiked) return `Você e ${likes[0]["user.username"]} curtiram`;
 				tmpTooltip = tmpTooltip + `e ${likes[1]["user.username"]} curtiram`;
 				return tmpTooltip;
-			}
-			else {
+			} else {
 				newTooltipNumber = tooltipNumber - 2;
 				tmpTooltip = `${tmpTooltip}, ${likes[1]["user.username"]} e mais ${newTooltipNumber} `;
 				moreUsersLiked(tmpTooltip, tooltipNumber - 2);
 			}
-		}
-		else {
+		} else {
 			tmpTooltip = tmpTooltip + "curtiu";
 			return tmpTooltip;
 		}
 		if (likes.length !== newTooltipNumber) {
-			if (tooltipNumber > 1)
-				tmpTooltip = tmpTooltip + "curtiram";
+			if (tooltipNumber > 1) tmpTooltip = tmpTooltip + "curtiram";
 			return tmpTooltip;
 		}
 	}
@@ -85,17 +86,12 @@ export default function Post({ postData }) {
 		if (!isLiked) {
 			setIsLiked(true);
 			setLikesNumber(likesNumber + 1);
-			likePost({ likedPost: id, token: loggedUser.token })
-				.then((resp) => console.log(resp.data))
-				.catch((err) => console.log(err.response))
+			likePost({ likedPost: id, token: loggedUser.token });
 			setLikesNumber(likesNumber + 1);
-		}
-		else {
-			setIsLiked(false)
+		} else {
+			setIsLiked(false);
 			setLikesNumber(likesNumber - 1);
-			dislikePost({ likedPost: id, token: loggedUser.token })
-				.then((resp) => console.log(resp.data))
-				.catch((err) => console.log(err.response))
+			dislikePost({ likedPost: id, token: loggedUser.token });
 		}
 	}
 
@@ -204,16 +200,13 @@ export default function Post({ postData }) {
 					</p>
 				)}
 
-
 				<PreviewContainer href={link} target="_blank" rel="noreferrer">
 					<DetailsContainer>
 						<div>
 							<h1>{linkTitle} </h1>
 							<p>{linkDescription}</p>
 						</div>
-						<div className="link-container">
-							{link}
-						</div>
+						<div className="link-container">{link}</div>
 					</DetailsContainer>
 					{linkImage ? (
 						<PostImage src={linkImage} />
@@ -224,60 +217,58 @@ export default function Post({ postData }) {
 						</LogoContainer>
 					)}
 				</PreviewContainer>
-
 			</RightContainer>
-
 		</PostContainer>
 	);
 }
 
 const PostContainer = styled.div`
-  width: 100%;
-  background-color: #171717;
-  border-radius: 16px;
-  padding: 20px 20px 20px 0;
-  display: flex;
-  margin-bottom: 15px;
+	width: 100%;
+	background-color: #171717;
+	border-radius: 16px;
+	padding: 20px 20px 20px 0;
+	display: flex;
+	margin-bottom: 15px;
 
-  button {
-    background: none;
-    border: none;
-  }
+	button {
+		background: none;
+		border: none;
+	}
 
-  @media (max-width: 611px) {
-    border-radius: 0px;
-  }
+	@media (max-width: 611px) {
+		border-radius: 0px;
+	}
 `;
 
 const LeftContainer = styled.div`
-  height: 100%;
-  width: 15%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+	height: 100%;
+	width: 15%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 
-  p {
-    font-size: 11px;
-    color: #fff;
-  }
+	p {
+		font-size: 11px;
+		color: #fff;
+	}
 
-  @media (max-width: 611px) {
-    p {
-      font-size: 9px;
-    }
-  }
+	@media (max-width: 611px) {
+		p {
+			font-size: 9px;
+		}
+	}
 `;
 
 const ProfilePicture = styled.img`
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  margin-bottom: 15px;
+	width: 50px;
+	height: 50px;
+	border-radius: 50%;
+	margin-bottom: 15px;
 
-  @media (max-width: 611px) {
-    height: 40px;
-    width: 40px;
-  }
+	@media (max-width: 611px) {
+		height: 40px;
+		width: 40px;
+	}
 `;
 
 const HeartOutline = styled(AiOutlineHeart)`
@@ -286,190 +277,189 @@ const HeartOutline = styled(AiOutlineHeart)`
 	color: #fff;
 	margin-bottom: 3px;
 
-@media(max-width: 611px) {
-	width: 17px;
-	height: 17px;
-}
+	@media (max-width: 611px) {
+		width: 17px;
+		height: 17px;
+	}
 `;
 
 const HeartFilled = styled(AiFillHeart)`
-width: 23px;
-height: 23px;
-color: #fff;
-margin-bottom: 3px;
+	width: 23px;
+	height: 23px;
+	color: #fff;
+	margin-bottom: 3px;
 
-@media(max-width: 611px) {
-	width: 17px;
-	height: 17px;
-}
+	@media (max-width: 611px) {
+		width: 17px;
+		height: 17px;
+	}
 `;
 
 const UserContainer = styled.div`
-font-size: 19px;
-color: rgb(255, 255, 255);
-margin-bottom: 12px;
-display: flex;
-justify-content: space-between;
+	font-size: 19px;
+	color: rgb(255, 255, 255);
+	margin-bottom: 12px;
+	display: flex;
+	justify-content: space-between;
 `;
 
 const RightContainer = styled.div`
-  height: 100%;
-  width: 83%;
+	height: 100%;
+	width: 83%;
 
-  a {
-	color: rgb(234, 232, 232);
-	text-decoration: none;
-	margin-bottom: 10px;
-	cursor: pointer;
+	a {
+		color: rgb(234, 232, 232);
+		text-decoration: none;
+		margin-bottom: 10px;
+		cursor: pointer;
 
-	:hover {
-	  color: rgb(255, 255, 255);
+		:hover {
+			color: rgb(255, 255, 255);
+		}
 	}
-  }
-
-  h2 {
-	font-size: 19px;
-	color: #fff;
-  }
-
-  > p {
-	font-size: 17px;
-	color: #aaaaaa;
-	line-height: 18px;
-	margin-bottom: 15px;
-	word-wrap: break-word;
-  }
-
-  @media (max-width: 611px) {
-	width: calc(100% - 30px);
 
 	h2 {
-	  font-size: 17px;
-	  margin-left: 5px;
+		font-size: 19px;
+		color: #fff;
 	}
 
-	p {
-	  font-size: 15px;
-	  margin-left: 5px;
+	> p {
+		font-size: 17px;
+		color: #aaaaaa;
+		line-height: 18px;
+		margin-bottom: 15px;
+		word-wrap: break-word;
 	}
-  }
+
+	@media (max-width: 611px) {
+		width: calc(100% - 30px);
+
+		h2 {
+			font-size: 17px;
+			margin-left: 5px;
+		}
+
+		p {
+			font-size: 15px;
+			margin-left: 5px;
+		}
+	}
 `;
 
 const PreviewContainer = styled.a`
-max-width: 503px;
-display: flex;
-justify-content: space-between;
+	max-width: 503px;
+	display: flex;
+	justify-content: space-between;
 `;
 
 const PostImage = styled.img`
-height: 155px;
-width: 153px;
-border-radius: 0 11px 11px 0;
+	height: 155px;
+	width: 153px;
+	border-radius: 0 11px 11px 0;
 
-@media(max-width: 611px) {
-	height: 115px;
-	width: 95px;
-}
+	@media (max-width: 611px) {
+		height: 115px;
+		width: 95px;
+	}
 `;
 
 const DetailsContainer = styled.div`
-height: 155px;
-border: 1px solid #c4c4c4;
-border-right: none;
-border-radius: 11px 0 0 11px;
-padding: 20px;
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-flex: 1 1 auto;
-overflow-y: hidden;
+	height: 155px;
+	border: 1px solid #c4c4c4;
+	border-right: none;
+	border-radius: 11px 0 0 11px;
+	padding: 20px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	flex: 1 1 auto;
+	overflow-y: hidden;
 
 	h1 {
-	font-size: 16px;
-	color: #cecece;
-	margin-bottom: 8px;
-}
+		font-size: 16px;
+		color: #cecece;
+		margin-bottom: 8px;
+	}
 
 	p {
-	font-size: 11px;
-	color: #aaaaaa;
-	margin: 10px 0;
-}
+		font-size: 11px;
+		color: #aaaaaa;
+		margin: 10px 0;
+	}
 
 	.link-container {
-	width: 100%;
-	white-space: nowrap;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	color: #cecece;
-	font-size: 9px;
-}
+		width: 100%;
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		color: #cecece;
+		font-size: 9px;
+	}
 
 	a {
-	text-decoration: none;
-	font-size: 11px;
-	color: #cecece;
-}
+		text-decoration: none;
+		font-size: 11px;
+		color: #cecece;
+	}
 
-@media(max-width: 611px) {
-	width: calc(100 % - 115px);
-	height: 115px;
-	padding: 10px;
+	@media (max-width: 611px) {
+		width: calc(100 % - 115px);
+		height: 115px;
+		padding: 10px;
 
 		h1 {
-		font-size: 11px;
-	}
+			font-size: 11px;
+		}
 
 		p {
-		font-size: 9px;
-	}
+			font-size: 9px;
+		}
 
 		a {
-		font-size: 9px;
+			font-size: 9px;
+		}
 	}
-}
 `;
 
 const LogoContainer = styled.div`
-height: 155px;
-width: 153px;
-border-radius: 0 11px 11px 0;
-background-color: #fff;
-display: flex;
-justify-content: center;
-align-items: center;
+	height: 155px;
+	width: 153px;
+	border-radius: 0 11px 11px 0;
+	background-color: #fff;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 
-@media(max-width: 611px) {
-	height: 115px;
-	width: 95px;
-}
+	@media (max-width: 611px) {
+		height: 115px;
+		width: 95px;
+	}
 `;
 
 const Logo = styled.div`
-font-family: "Passion One", cursive;
-font-size: 35px;
-font-weight: 700;
-color: #000;
+	font-family: "Passion One", cursive;
+	font-size: 35px;
+	font-weight: 700;
+	color: #000;
 `;
 
 const InputText = styled.textarea`
-width: 100%;
-font-size: 17px;
-font-family: "Lato", sans - serif;
-background-color: ${({ loading }) =>
-		loading ? "rgb(242, 242, 242)" : "rgb(255,255,255)"
-	};
-word-wrap: break-word;
-border-radius: 7px;
-margin-bottom: 5px;
-padding: 8px;
-resize: vertical;
+	width: 100%;
+	font-size: 17px;
+	font-family: "Lato", sans - serif;
+	background-color: ${({ loading }) =>
+		loading ? "rgb(242, 242, 242)" : "rgb(255,255,255)"};
+	word-wrap: break-word;
+	border-radius: 7px;
+	margin-bottom: 5px;
+	padding: 8px;
+	resize: vertical;
 
-  :focus {
-	outline: none!important;
-}
+	:focus {
+		outline: none !important;
+	}
 
-@media(max-width: 611px) {
-	font-size: 15px;
-}
+	@media (max-width: 611px) {
+		font-size: 15px;
+	}
 `;
