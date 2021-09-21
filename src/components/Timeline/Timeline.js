5fe4b2332344ade99c26e76bb8ebc9ac2650f427
giancarvalho <giandcarvalho@gmail.com>
2021-09-21 15:41:24 -0300

    finished layout and downloaded react debounce input package

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -11,54 +11,58 @@ import { getPosts } from "../../API/requests";
 import routes from "../../routes/routes";
 import { useEffect, useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
+import Search from "../Header/Search";
 
 export default function Timeline() {
-	const { loggedUser } = useContext(UserContext);
-	const { pagePosts, setPagePosts } = useContext(PagePostsContext);
-	const history = useHistory();
-	const [loading, setLoading] = useState(true);
+  const { loggedUser } = useContext(UserContext);
+  const { pagePosts, setPagePosts } = useContext(PagePostsContext);
+  const history = useHistory();
+  const [loading, setLoading] = useState(true);
 
-	function updateTimeline() {
-		if (!loggedUser.token) return history.push(routes.login);
+  function updateTimeline() {
+    if (!loggedUser.token) return history.push(routes.login);
 
-		setPagePosts([]);
+    setPagePosts([]);
 
-		getPosts(loggedUser)
-			.then((resp) => {
-				if (resp.data.posts.length === 0) alert("Nenhum post encontrado");
-				setPagePosts(resp.data.posts);
+    getPosts(loggedUser)
+      .then((resp) => {
+        if (resp.data.posts.length === 0) alert("Nenhum post encontrado");
+        setPagePosts(resp.data.posts);
 
-				setLoading(false);
-			})
-			.catch(() => {
-				alert("Houve uma falha ao obter os posts, por favor atualize a página");
-				setLoading(false);
-			});
-	}
+        setLoading(false);
+      })
+      .catch(() => {
+        alert("Houve uma falha ao obter os posts, por favor atualize a página");
+        setLoading(false);
+      });
+  }
 
-	useEffect(() => updateTimeline(), []);
+  useEffect(() => updateTimeline(), []);
 
-	return (
-		<PageContainer>
-			{loading ? (
-				<CircleLoader customStyle={{ height: "50vh" }} />
-			) : (
-				<>
-					<Header />
+  return (
+    <PageContainer>
+      {loading ? (
+        <CircleLoader customStyle={{ height: "50vh" }} />
+      ) : (
+        <>
+          <Header />
 
-					<ContentContainer>
-						<PageTitleContainer><h1>timeline</h1></PageTitleContainer>
+          <ContentContainer>
+            <Search className="timeline" />
+            <PageTitleContainer>
+              <h1>timeline</h1>
+            </PageTitleContainer>
 
-						<PublishBox updateTimeline={updateTimeline} />
+            <PublishBox updateTimeline={updateTimeline} />
 
-						{pagePosts.map((post) => (
-							<Post postData={post} key={post.id} />
-						))}
-					</ContentContainer>
+            {pagePosts.map((post) => (
+              <Post postData={post} key={post.id} />
+            ))}
+          </ContentContainer>
 
-					<HashtagBox />
-				</>
-			)}
-		</PageContainer>
-	);
+          <HashtagBox />
+        </>
+      )}
+    </PageContainer>
+  );
 }
