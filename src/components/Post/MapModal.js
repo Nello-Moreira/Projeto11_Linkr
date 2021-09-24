2dfd4f0ab4efd617edb3e692beba18be6aa60ac7
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-09-24 09:16:45 -0300

    Fixed leaflet install problem

--- src/components/Post/MapModal.js
+++ src/components/Post/MapModal.js
@@ -2,29 +2,23 @@ import Modal from "../Modal/Modal";
 import ModalTitle from "../Modal/ModalTitle";
 import { AiOutlineClose } from "react-icons/ai";
 import styled from "styled-components";
-import GoogleMapReact from "google-map-react";
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 export default function MapModal({ isOpen, username, setIsOpen, geolocation }) {
-	const handleApiLoaded = (map, maps) => {
-		// use map and maps objects
-	};
+	const position = [geolocation.latitude, geolocation.longitude];
 	return (
 		<Modal isOpen={isOpen}>
 			<ModalHeader>
 				{username}'s Location{" "}
 				<AiOutlineClose onClick={() => setIsOpen(false)} />
 			</ModalHeader>
-			{geolocation.latitude}
-			{geolocation.longitude}
-			<GoogleMapReact>
-				<AnyReactComponent
-					lat={geolocation.latitude}
-					lng={geolocation.longitude}
-					text="My Marker"
+			<MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+				<TileLayer
+					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				/>
-			</GoogleMapReact>
+				<Marker position={position}></Marker>
+			</MapContainer>
 		</Modal>
 	);
 }
