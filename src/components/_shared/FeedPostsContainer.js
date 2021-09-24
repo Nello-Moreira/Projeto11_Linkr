fe7aac1fb46b15ea79441d22bf3133be64c6251d
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-09-24 18:46:52 -0300

    Fixed images with load error
    That was a bug on on treating snippets images loading errors that
    is fixed now. Besides that, avatar images with loading errors have
    been fixed too.

--- src/components/_shared/FeedPostsContainer.js
+++ src/components/_shared/FeedPostsContainer.js
@@ -9,146 +9,142 @@ import { useEffect, useContext, useState } from "react";
 import useInterval from "use-interval";
 
 function getNewPosts({ APIfunction, settings, pagePosts }) {
-    return APIfunction(settings)
-        .then((response) => {
-            const posts = response.data.posts;
+	return APIfunction(settings)
+		.then((response) => {
+			const posts = response.data.posts;
 
-            if (posts.length === 0 || pagePosts.length === 0) return posts;
+			if (posts.length === 0 || pagePosts.length === 0) return posts;
 
-            const index = indexOfLastPost(posts, pagePosts[0]);
+			const index = indexOfLastPost(posts, pagePosts[0]);
 
-            console.log({ pagePosts });
-            if (index > -1) {
-                console.log({ posts: posts.slice(0, index) });
-                return posts.slice(0, index);
-            }
-            return [
-                ...posts,
-                APIfunction({
-                    ...settings,
-                    lastPostId: posts[index].id,
-                }),
-            ];
-        })
-        .catch((error) => error);
+			console.log({ pagePosts });
+			if (index > -1) return posts.slice(0, index);
+
+			return [
+				...posts,
+				APIfunction({
+					...settings,
+					lastPostId: posts[index].id,
+				}),
+			];
+		})
+		.catch((error) => error);
 }
 
 function FeedPostsContainer({ APIfunction, settings }) {
-    const { loggedUser } = useContext(UserContext);
-    const { token } = loggedUser;
-    const { pagePosts, setPagePosts } = useContext(PagePostsContext);
-    const [loading, setLoading] = useState(true);
-    const [lastPostId, setLastPostId] = useState(null);
-    const [hasMore, setHasMore] = useState(true);
-    const intervalInSeconds = 10;
+	const { loggedUser } = useContext(UserContext);
+	const { token } = loggedUser;
+	const { pagePosts, setPagePosts } = useContext(PagePostsContext);
+	const [loading, setLoading] = useState(true);
+	const [lastPostId, setLastPostId] = useState(null);
+	const [hasMore, setHasMore] = useState(true);
+	const intervalInSeconds = 10;
 
-    function updateFeed(settings) {
-        settings = { token, lastPostId, ...settings } || { token, lastPostId };
+	function updateFeed(settings) {
+		settings = { token, lastPostId, ...settings } || { token, lastPostId };
 
-        APIfunction(settings)
-            .then((response) => {
-                const posts = response.data.posts;
+		APIfunction(settings)
+			.then((response) => {
+				const posts = response.data.posts;
 
-                if (posts.length < 10) {
-                    setHasMore(false);
-                }
+				if (posts.length < 10) {
+					setHasMore(false);
+				}
 
-                if (!lastPostId) {
-                    setPagePosts(posts);
-                } else {
-                    setPagePosts([...pagePosts, ...posts]);
-                }
+				if (!lastPostId) {
+					setPagePosts(posts);
+				} else {
+					setPagePosts([...pagePosts, ...posts]);
+				}
 
-                if (posts.length > 0) {
-                    setLastPostId(posts[posts.length - 1].id);
-                }
-                setLoading(false);
-            })
-            .catch((error) => {
-                alert(
-                    "Houve uma falha ao obter os posts, por favor atualize a página"
-                );
-                setLoading(false);
-            });
-    }
+				if (posts.length > 0) {
+					setLastPostId(posts[posts.length - 1].id);
+				}
+				setLoading(false);
+			})
+			.catch((error) => {
+				alert("Houve uma falha ao obter os posts, por favor atualize a página");
+				setLoading(false);
+			});
+	}
 
-    useEffect(() => {
-        updateFeed(settings);
-    }, [APIfunction]);
+	useEffect(() => {
+		updateFeed(settings);
+	}, [APIfunction]);
 
-    useInterval(() => {
-        settings = { token, ...settings };
+	useInterval(() => {
+		settings = { token, ...settings };
 
-        getNewPosts({ APIfunction, settings, pagePosts })
-            .then((response) => {
-                setPagePosts([...response, ...pagePosts]);
-            })
-            .catch(
-                (error) => console.log(error)
-                /* alert(
+		getNewPosts({ APIfunction, settings, pagePosts })
+			.then((response) => {
+				setPagePosts([...response, ...pagePosts]);
+			})
+			.catch(
+				(error) => console.log(error)
+				/* alert(
                     "Não foi possível carregar os novos posts. Por favor, recarregue a página."
                 ) */
-            );
-    }, intervalInSeconds * 1000);
+			);
+	}, intervalInSeconds * 1000);
 
-    return loading ? (
-        <FeedLoader />
-    ) : pagePosts.length === 0 ? (
-        <WarningParagraph>Nenhuma publicação encontrada</WarningParagraph>
-    ) : (
-        <InfiniteTimeline
-            pageStart={0}
-            loadMore={() => updateFeed(settings)}
-            hasMore={hasMore}
-            loader={<FeedLoader />}
-        >
-            {pagePosts.map((post) => (
-                <Post postData={post} key={JSON.stringify(post)} />
-            ))}
-        </InfiniteTimeline>
-    );
+	return loading ? (
+		<FeedLoader />
+	) : pagePosts.length === 0 ? (
+		<WarningParagraph>Nenhuma publicação encontrada</WarningParagraph>
+	) : (
+		<InfiniteTimeline
+			pageStart={0}
+			loadMore={() => updateFeed(settings)}
+			hasMore={hasMore}
+			loader={<FeedLoader />}
+		>
+			{pagePosts.map((post) => (
+				<Post postData={post} key={JSON.stringify(post)} />
+			))}
+		</InfiniteTimeline>
+	);
 }
 
 function indexOfLastPost(postsList, postToCompare) {
-    let index = -1;
+	let index = -1;
 
-    postsList.forEach((post, i) => {
-        if (index > -1) return;
+	postsList.forEach((post, i) => {
+		if (index > -1) return;
 
-        if (
-            post.id === postToCompare.id &&
-            post.repostId === postToCompare.repostId
-        ) {
-            index = i;
-        }
-    });
+		if (
+			post.id === postToCompare.id &&
+			post.repostId === postToCompare.repostId
+		) {
+			index = i;
+		}
+	});
 
-    return index;
+	return index;
 }
 
 function FeedLoader() {
-    return (
-        <LoaderContainer>
-            <CircleLoader customStyle={{ color: "rgba(109, 109, 109, 1)" }} />
-            <p>Carregando mais posts...</p>
-        </LoaderContainer>
-    );
+	return (
+		<LoaderContainer>
+			<CircleLoader customStyle={{ color: "rgba(109, 109, 109, 1)" }} />
+			<p>Carregando mais posts...</p>
+		</LoaderContainer>
+	);
 }
 
 const InfiniteTimeline = styled(InfiniteScroll)`
-    width: 100%;
+	width: 100%;
 `;
 
 const LoaderContainer = styled.div`
-    font-family: "Lato", sans-serif;
-    font-size: 20px;
-    font-weight: 400;
-    color: rgba(109, 109, 109, 1);
-    height: 30vh;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+	font-family: "Lato", sans-serif;
+	font-size: 20px;
+	font-weight: 400;
+	color: rgba(109, 109, 109, 1);
+	height: 30vh;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 `;
 
 export { FeedPostsContainer, getNewPosts };
--- src/components/_shared/UserAvatar.js
+++ src/components/_shared/UserAvatar.js
@@ -1,40 +1,45 @@
-import { useContext } from "react";
+import { useState } from "react";
 import styled from "styled-components";
-import UserContext from "../../contexts/UserContext";
+import defaultUserImage from "../../assets/images/defaultUserImage.jpg";
 
 export default function UserAvatar({ customStyle = {}, src }) {
-  const { loggedUser } = useContext(UserContext);
+	const [imgIsLoaded, setImgIsLoaded] = useState(true);
 
-  return (
-    <Profile customStyle={customStyle}>
-      <img src={src ? src : loggedUser.user.avatar} alt="userAvatar" />
-    </Profile>
-  );
+	return (
+		<Profile customStyle={customStyle}>
+			<img
+				src={imgIsLoaded ? src : defaultUserImage}
+				onError={() => setImgIsLoaded(false)}
+				onLoad={() => console.log("carregou")}
+				alt="userAvatar"
+			/>
+		</Profile>
+	);
 }
 
 const Profile = styled.div`
-  padding: ${({ customStyle }) => customStyle.padding || "inherit"};
-  margin: ${({ customStyle }) => customStyle.margin || 0};
-  display: flex;
-  align-items: center;
+	padding: ${({ customStyle }) => customStyle.padding || "inherit"};
+	margin: ${({ customStyle }) => customStyle.margin || 0};
+	display: flex;
+	align-items: center;
 
-  @media (max-width: 611px) {
-    display: ${({ customStyle }) => customStyle.mobileDisplay || "initial"};
-  }
+	@media (max-width: 611px) {
+		display: ${({ customStyle }) => customStyle.mobileDisplay || "initial"};
+	}
 
-  img {
-    object-fit: cover;
-    width: ${({ customStyle }) =>
-      customStyle.width ? customStyle.width : "50px"};
-    height: ${({ customStyle }) =>
-      customStyle.height ? customStyle.height : "50px"};
-    border-radius: 50%;
+	img {
+		object-fit: cover;
+		width: ${({ customStyle }) =>
+			customStyle.width ? customStyle.width : "50px"};
+		height: ${({ customStyle }) =>
+			customStyle.height ? customStyle.height : "50px"};
+		border-radius: 50%;
 
-    @media (max-width: 611px) {
-      height: ${({ customStyle }) =>
-        customStyle.resizeOnMobile === true ? "40px" : "50px"};
-      width: ${({ customStyle }) =>
-        customStyle.resizeOnMobile === true ? "40px" : "50px"};
-    }
-  }
+		@media (max-width: 611px) {
+			height: ${({ customStyle }) =>
+				customStyle.resizeOnMobile === true ? "40px" : "50px"};
+			width: ${({ customStyle }) =>
+				customStyle.resizeOnMobile === true ? "40px" : "50px"};
+		}
+	}
 `;
