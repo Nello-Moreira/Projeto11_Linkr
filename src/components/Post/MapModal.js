78ca375885254acc17c1dc9ab4dd9d4f180cdcfa
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-09-24 10:50:47 -0300

    Added map

--- src/components/Post/MapModal.js
+++ src/components/Post/MapModal.js
@@ -2,7 +2,21 @@ import Modal from "../Modal/Modal";
 import ModalTitle from "../Modal/ModalTitle";
 import { AiOutlineClose } from "react-icons/ai";
 import styled from "styled-components";
+import Leaflet from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import iconRetina from "leaflet/dist/images/marker-icon-2x.png";
+
+let DefaultIcon = Leaflet.icon({
+	...Leaflet.Icon.Default.prototype.options,
+	iconUrl: icon,
+	iconRetinaUrl: iconRetina,
+	shadowUrl: iconShadow,
+});
+Leaflet.Marker.prototype.options.icon = DefaultIcon;
 
 export default function MapModal({ isOpen, username, setIsOpen, geolocation }) {
 	const position = [geolocation.latitude, geolocation.longitude];
@@ -12,13 +26,13 @@ export default function MapModal({ isOpen, username, setIsOpen, geolocation }) {
 				{username}'s Location{" "}
 				<AiOutlineClose onClick={() => setIsOpen(false)} />
 			</ModalHeader>
-			<MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+			<Map center={position} zoom={10} scrollWheelZoom={false}>
 				<TileLayer
 					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				/>
 				<Marker position={position}></Marker>
-			</MapContainer>
+			</Map>
 		</Modal>
 	);
 }
@@ -30,4 +44,10 @@ const ModalHeader = styled(ModalTitle)`
 	font-family: Oswald;
 	font-weight: 700;
 	font-size: 38px;
+	margin-bottom: 15px;
+`;
+
+const Map = styled(MapContainer)`
+	width: 100%;
+	height: 300px;
 `;
