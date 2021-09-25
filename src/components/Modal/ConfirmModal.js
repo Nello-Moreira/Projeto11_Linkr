5b0a560bfd36486fba15ed8e97fc379778362653
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-09-25 13:52:55 -0300

    Created a gerenic confirmation modal

---
+++ src/components/Modal/ConfirmModal.js
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+import Modal from "../Modal/Modal";
+import ModalTitle from "../Modal/ModalTitle";
+import ButtonsContainer from "../_shared/buttons/ButtonsContainer";
+import CustomButton from "../_shared/buttons/CustomButton";
+
+export default function ConfirmModal({
+	isOpen,
+	setIsOpen,
+	loading,
+	onConfirm,
+	onCancel,
+	title,
+	confirmText,
+	cancelText,
+}) {
+	function handleKeys(e) {
+		console.log(e);
+		if (e.key === "Enter") {
+			e.preventDefault();
+
+			if (e.repeat) {
+				return;
+			}
+			onConfirm();
+		}
+	}
+
+	return (
+		<ModalContainer isOpen={isOpen} onRequestClose={() => setIsOpen(false)}>
+			<Title>{title}</Title>
+
+			<ButtonsContainer
+				customStyle={{ width: "100%", separationMargin: "45px 10px 15px" }}
+			>
+				<CancelButton
+					customStyle={{
+						loading,
+					}}
+					onClick={
+						loading
+							? null
+							: () => {
+									setIsOpen(false);
+									if (onCancel) onCancel();
+							  }
+					}
+				>
+					{cancelText ? cancelText : "Não, cancelar"}
+				</CancelButton>
+
+				<ConfirmButton
+					type="submit"
+					customStyle={{ loading }}
+					onClick={() => {
+						setIsOpen(false);
+						onConfirm();
+					}}
+					onKeyDown={handleKeys}
+				>
+					{confirmText ? confirmText : "Sim, confirmar"}
+				</ConfirmButton>
+			</ButtonsContainer>
+		</ModalContainer>
+	);
+}
+
+const CancelButton = styled(CustomButton)`
+	background-color: rgb(225, 225, 225);
+	color: rgb(24, 119, 242);
+`;
+
+const ConfirmButton = styled(CustomButton)``;
+
+const ModalContainer = styled(Modal)`
+	border-radius: 50px;
+`;
+
+const Title = styled(ModalTitle)`
+	text-align: center;
+`;
