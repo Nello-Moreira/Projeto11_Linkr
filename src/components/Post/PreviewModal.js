b281fa2200245f9ffe3671b70eaa7ce06f47a8f5
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-09-24 21:19:13 -0300

    Changed mapModal close btn to close when pressing esc

--- src/components/Post/PreviewModal.js
+++ src/components/Post/PreviewModal.js
@@ -1,46 +1,45 @@
 import styled from "styled-components";
 import Modal from "../Modal/Modal";
-import { AiOutlineClose } from "react-icons/ai";
 import CustomButton from "../_shared/buttons/CustomButton";
 import CloseButton from "../_shared/buttons/CloseButton";
 
 export default function PreviewModal({ link, linkTitle, isOpen, setIsOpen }) {
-    return (
-        <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)}>
-            <ModalHeader>
-                <a href={link} target="_blank" rel="noreferrer">
-                    <NewTabButton> Open in a new tab</NewTabButton>
-                </a>
-                <CloseButton onClick={() => setIsOpen(false)} />
-            </ModalHeader>
-            <Preview src={link} title={linkTitle}></Preview>
-        </Modal>
-    );
+	return (
+		<Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)}>
+			<ModalHeader>
+				<a href={link} target="_blank" rel="noreferrer">
+					<NewTabButton> Open in a new tab</NewTabButton>
+				</a>
+				<CloseButton onClick={() => setIsOpen(false)} />
+			</ModalHeader>
+			<Preview src={link} title={linkTitle}></Preview>
+		</Modal>
+	);
 }
 
 const ModalHeader = styled.div`
-    width: 100%;
-    margin-bottom: 15px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+	width: 100%;
+	margin-bottom: 15px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
 
-    a {
-        text-decoration: none;
-    }
+	a {
+		text-decoration: none;
+	}
 `;
 
 const NewTabButton = styled(CustomButton)`
-    font-family: Lato;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 14px;
+	font-family: Lato;
+	font-style: normal;
+	font-weight: bold;
+	font-size: 14px;
 
-    width: 138px;
-    height: 31px;
+	width: 138px;
+	height: 31px;
 `;
 
 const Preview = styled.iframe`
-    width: 100%;
-    height: 500px;
+	width: 100%;
+	height: 500px;
 `;
